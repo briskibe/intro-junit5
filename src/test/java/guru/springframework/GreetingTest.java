7210641aa6b2c09d922b7105d1ee8d9e72ab4bdb
briskibe <bernard.briski@analyticom.de>
2023-07-11 18:08:18 +0200

    Added other lifecycle methods

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,10 +1,19 @@
 package guru.springframework;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GreetingTest {
     private Greeting greeting;
+
+    // must be static
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("In Before All...");
+    }
 
     @BeforeEach
     void setUp() {
@@ -20,5 +29,15 @@ class GreetingTest {
     @Test
     void testHelloWorld() {
         System.out.println(greeting.helloWorld("Bernard"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("In After Each...");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("In After All...");
     }
 }
