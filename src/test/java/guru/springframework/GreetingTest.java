0426318c65e255e3d863b7feb3f372b4fa26a90b
briskibe <bernard.briski@analyticom.de>
2023-07-11 18:05:34 +0200

    Initializing new Greeting object in new BeforeEach method

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,18 +1,24 @@
 package guru.springframework;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GreetingTest {
+    private Greeting greeting;
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("In Before Each...");
+        greeting = new Greeting();
+    }
 
     @Test
     void helloWorld() {
-        Greeting greeting = new Greeting();
         System.out.println(greeting.helloWorld());
     }
 
     @Test
     void testHelloWorld() {
-        Greeting greeting = new Greeting();
         System.out.println(greeting.helloWorld("Bernard"));
     }
 }
